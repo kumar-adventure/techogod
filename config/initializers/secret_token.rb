ac207191158c906b31b88977d3a207845272d44a
kumar <kumar.adventure@gmail.com>
2014-03-10 23:36:27 +0530

    create application with rails4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Techogod::Application.config.secret_key_base = 'cc8f10be397dd26934ab8c178626e44d82f2160715edd203dde43cdb6e246da05709b45dc12bea01b691ac3689b169cf6089ba997b8ceb1fb63073cca23fce03'
