6d5c94b24e5f417973a6393d0fe29989665312cc
kumar <kumar.adventure@gmail.com>
2014-03-20 00:43:41 +0530

    add google map at home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    result = request.location
+    if Rails.env == "production"
+    @lat = result.latitude
+    @lang = result.longitude
+    else
+    @lat =  27.175015
+    @lang = 78.042155
+    end
+    
     redirect_to new_user_session_path unless current_user
   end
 end
